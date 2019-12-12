65fa86dca57025ca15922af70a1df7aec84edb41
RicardoSP018 <ricardo.sp018@gmail.com>
2019-12-12 09:43:58 -0600

    Version 3

--- tests/Unit/PeriodoPaqueteTest.php
+++ tests/Unit/PeriodoPaqueteTest.php
@@ -14,7 +14,19 @@ class PeriodoPaqueteTest extends TestCase
      * @return void
      */
     public function PeriodoPaqueteTest()
+
     {
+         $fechaInicio='2019-08-17';
+         $intervalo='P7D';
+        $fechaFinal='2019-08-25';
+        
+        $response=$this->
+        json('GET',
+        '/api/periodopaquete/'.$fechaInicio.'/'.$fechaFinal);
+        $response->assertStatus(200);//Si hay respuesta
+        $response->assertJson(['r'=>20]);
+        
         $this->assertTrue(true);
+
     }
 }
